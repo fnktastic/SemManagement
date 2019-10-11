57ba3fe484bff951894ca4e1573b16aec5d97448
Oleksandr Herhelezhyu <olek.h@outlook.com>
2019-10-12 02:21:09 +0300

    Added Controllers and RestSharp

---
+++ SemManagement.UWP/Configurations/PublicApiConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemManagement.UWP.Configurations
+{
+    public class PublicApiConfiguration
+    {
+        public string BaseUrl { get; set; }
+    }
+}
